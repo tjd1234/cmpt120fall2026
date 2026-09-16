23e95e1d60f72cfdbcc5e7a2ae1b66dcea658f18
Toby Donaldson <tjd@sfu.ca>
2026-09-16 11:45:48 -0700

    updated

---
+++ code/lego.py
@@ -0,0 +1,5 @@
+# lego.py
+
+print('Patricia bought',
+      1*663 + 2*2295 + 3*210, # calculate number of pieces
+      'Lego pieces.')
---
+++ code/tahiti.py
@@ -0,0 +1,4 @@
+# tahiti.py
+
+print('The average temperature was',
+      (81.5 + 82 + 83 + 84 + 79.5) / 5, 'Fahrenheit.')
